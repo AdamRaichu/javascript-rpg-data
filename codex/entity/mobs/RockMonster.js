877dd5b9aee1d40dc738e75af0391312b3559404
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-03-19 15:47:26 -0400

    fix the rest of the imports (for browser)

--- codex/entity/mobs/RockMonster.js
+++ codex/entity/mobs/RockMonster.js
@@ -1,5 +1,5 @@
-import { Entity } from "../Entity";
-import { Resistance1 } from "../../effect/effects/Resistance";
+import { Entity } from "../Entity.js";
+import { Resistance1 } from "../../effect/effects/Resistance.js";
 
 export class RockMonster extends Entity {
   constructor() {
